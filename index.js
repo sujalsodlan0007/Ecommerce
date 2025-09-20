7682a0e0fe6c7478008762aadfc8370af455ff6f
sujalsodlan0007 <sujalsodlan0007@gmail.com>
2025-09-20 21:08:19 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv'
- import connectDB from './config/db.js'
+import connectDB from './config/db.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 dotenv.config()
  let port = process.env.PORT || 6000
 let app = express() 
 
- app.use(express.json())
- app.use(cookieParser())
+app.use(cors({
+  origin: 'http://localhost:8000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
+app.use(express.json())
+app.use(cookieParser())
 import authRoutes from './routes/authRoutees.js'
 app.use("/api/auth", authRoutes)
 
